b9f47fa506c850bbac4f051d6cc0d1e612664d25
Howard Tai <howardtai86@gmail.com>
2020-11-04 23:07:53 +0800

    :tada: initial commit with Spring Initializr

---
+++ src/main/java/com/howardism/webscraping/WebScrapingApplication.java
@@ -0,0 +1,16 @@
+package com.howardism.webscraping;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class WebScrapingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebScrapingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/howardism/webscraping/WebScrapingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.howardism.webscraping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebScrapingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
